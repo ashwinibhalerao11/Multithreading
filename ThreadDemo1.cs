f23169152b5d800c663edffa5de176a863e4bfb2
ashwinibhalerao11 <98452094+ashwinibhalerao11@users.noreply.github.com>
2023-05-15 12:56:54 +0530

    Add files via upload

---
+++ ThreadDemo1.cs
@@ -0,0 +1,52 @@
+﻿using DotNet.Multithreading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Multithreading
+{
+    public class ThreadDemo1
+    {
+        public void Task1()
+        {
+            lock (this) // this refers to current thread
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+        public void Task2()
+        {
+            lock (this)
+            {
+                for (int i = 10; i <= 15; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            ThreadDemo threadDemo = new ThreadDemo();
+            Thread t1 = new Thread(new ThreadStart(threadDemo.Task1));
+            Thread t2 = new Thread(new ThreadStart(threadDemo.Task2));
+            Thread t3 = new Thread(new ThreadStart(threadDemo.Task1));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+
+
+    }
+}
