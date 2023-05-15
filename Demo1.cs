ae7364124c612ed019bcaf70557bcbfb92c0858d
ashwinibhalerao11 <98452094+ashwinibhalerao11@users.noreply.github.com>
2023-05-15 12:56:34 +0530

    Add files via upload

---
+++ Demo1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Multithreading
+{
+    public class Demo1
+    {
+        public void Task1()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+            }
+        }
+        public void Task2()
+        {
+            for (int i = 10; i <= 15; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            Demo1 threadDemo = new Demo1();
+            Thread t1 = new Thread(new ThreadStart(threadDemo.Task1));
+            Thread t2 = new Thread(new ThreadStart(threadDemo.Task2));
+            t1.Priority = ThreadPriority.Highest;
+            t2.Priority = ThreadPriority.Lowest;
+            t1.Start();
+            t1.Join(); // block the execution till t1 thread will complete the task
+            //OR
+            t1.Join(3000); // block the execution of another thread till 3000ms,after  the duration unblock the thread
+
+            t2.Start();
+
+        }
+    }
+}
---
+++ ThreadDemo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Multithreading
+{
+    public class ThreadDemo
+    {
+        public void Task1()
+        {
+            try
+            {
+                Monitor.Enter(this); // applied the lock
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                Monitor.Wait(this, 5000);
+                Monitor.Pulse(this);
+                Monitor.PulseAll(this);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                Monitor.Exit(this);// release the lock
+            }
+
+        }
+        public void Task2()
+        {
+            lock (this)
+            {
+                for (int i = 10; i <= 15; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            ThreadDemo threadDemo = new ThreadDemo();
+            Thread t1 = new Thread(new ThreadStart(threadDemo.Task1));
+            Thread t2 = new Thread(new ThreadStart(threadDemo.Task2));
+            Thread t3 = new Thread(new ThreadStart(threadDemo.Task1));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
